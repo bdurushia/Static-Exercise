22f068b5e56354ba0cd30a01946c82ae55e4d8b1
Bryan Durushia <b.durushia@gmail.com>
2024-03-01 11:54:34 -0600

    Completed Static Exercise (Temp converter).

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"51\u00B0 F converted to Celsius: {TempConverter.FahrenheitToCelcius(51)}\u00B0 C");
+
+            Console.WriteLine($"14\u00B0 C converted to Fahrenheit: {TempConverter.CelciusToFahrenheit(14)}\u00B0 F");
         }
     }
 }
